c45fcfb81dec2ff96d3a7926f66aba51d579c97d
tkahng <tkahng@gmail.com>
2021-02-05 22:28:45 +0900

    a

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+
 
 stdb = "data/streamlit.db"
 
@@ -13,16 +17,34 @@ def dfFromSql(dbpath, query, params=None):
     con.close()
     return df.convert_dtypes()
 
-materials = dfFromSql(stdb, "SELECT * FROM materials")
 
-categories = materials['ct_2_text'].drop_duplicates().tolist()
+def shapedata(df):
+    df = (
+        df
+        .groupby('mt_no mt_name'.split())
+        .agg(lambda x: list(set(list(x))))
+    )
+    return df
 
-def filterdata(selections=[]):
-    if len(selections) < 1:
-        return materials
+def filterdata(df, keys=[]):
+    if len(keys) < 1:
+        return df
     else:
-        return materials[materials['ct_2_text'].isin(selections)]
+        letters_s = set(keys)
+        return df[df['ct_2_text'].map(letters_s.issubset)]
 
-selection = st.sidebar.multiselect('select category', categories)
+def get_tags(df):
+    tags = df.explode('ct_2_text')['ct_2_text'].unique().tolist()
+    return tags
 
-st.write('You selected:', filterdata(selection))
+materials = dfFromSql(stdb, "SELECT * FROM testmaterial")
+
+data = shapedata(materials)
+
+# categories = materials['ct_2_text'].drop_duplicates().tolist()
+
+selection = st.multiselect('select category', get_tags(data))
+
+data = filterdata(data, selection)
+
+st.write('You selected:', data)
